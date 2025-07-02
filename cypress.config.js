73b8f493457f3cd8c3492119586948d78c48a36f
tkellys <thamireskellysantos@gmail.com>
2025-07-02 09:27:04 -0300

    reorganizando pastas

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'pjgihb',
   e2e: {
     baseUrl: 'https://qualeagiria.com.br/',
     setupNodeEvents(on, config) {
